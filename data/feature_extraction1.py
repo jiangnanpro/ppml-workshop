a51baeba1a9a2b130585af117652a54267361a42
jiangnan <jiangnan2217@gmail.com>
2021-08-23 13:24:14 +0200

    add script for generating table1

---
+++ data/feature_extraction1.py
@@ -0,0 +1,45 @@
+# Script to convert images into tabular data for QMNIST
+import pickle
+import os
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+
+from utils import load_data, resize_image
+
+
+if __name__ == '__main__':
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Load QMNIST data from QMNIST.pickle
+    pickle_file = os.path.join(current_dir, 'QMNIST_ppml.pickle')
+    x_defender, x_reserve, y_defender, y_reserve = load_data(pickle_file)
+    print('Data loaded.')
+
+    x_defender = resize_image(x_defender)
+    x_reserve = resize_image(x_reserve)
+
+    base_model = tf.keras.applications.VGG19(weights='imagenet',include_top=False)
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer('block5_pool').output)
+
+    x_defender_features = model.predict(x_defender)
+    x_reserve_features = model.predict(x_reserve)
+
+    x_defender_tabular = x_defender_features
+    x_reserve_tabular = x_reserve_features
+
+    x_defender_tabular = x_defender_tabular.squeeze()
+    x_reserve_tabular = x_reserve_tabular.squeeze()
+    print(x_defender_tabular.shape)
+    # Create dict of partitions
+
+    split_dict = dict()
+    split_dict['x_defender'] = x_defender_tabular
+    split_dict['x_reserve'] = x_reserve_tabular
+    split_dict['y_defender'] = y_defender
+    split_dict['y_reserve'] = y_reserve
+
+    # Store the dict using pickle
+    with open(os.path.join(current_dir, 'QMNIST_tabular_ppml.pickle'), 'wb') as f:
+        pickle.dump(split_dict, f)
+
+    print('Tabular data stored.')
---
+++ data/split_qmnist.py
@@ -0,0 +1,38 @@
+# Script to reproduce the split of QMNIST.pickle
+import pickle
+import os
+
+from torchvision.datasets import QMNIST # version 0.9.1
+import numpy as np # version 1.19.5
+
+if __name__=='__main__':
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Load QMNIST data from torchvision.datasets
+    qall = QMNIST('qmnist', what='nist', download=True, compat=False)
+
+    qmnist_images = qall.data.numpy()
+    qmnist_targets = qall.targets.numpy()
+
+    qmnist_len = qmnist_images.shape[0]
+
+    # Set the size of each partition of the split
+    defender_partition_size = 200000
+    reserve_partition_size = qmnist_len-200000
+
+    # Randomly selecting the images for each partition
+    rng = np.random.RandomState(2021)
+    random_seq = rng.choice(qmnist_len,size=qmnist_len, replace=False)
+
+    defender_partition_indexes = random_seq[0:defender_partition_size]
+    reserve_partition_indexes = random_seq[defender_partition_size:]
+
+    # Create dict of partitions
+    split_dict = dict()
+    split_dict['x_defender'] = qmnist_images[defender_partition_indexes]
+    split_dict['x_reserve'] = qmnist_images[reserve_partition_indexes]
+    split_dict['y_defender'] = qmnist_targets[defender_partition_indexes]
+    split_dict['y_reserve'] = qmnist_targets[reserve_partition_indexes]
+
+    # Store the dict using pickle
+    with open(os.path.join(current_dir,'QMNIST_ppml.pickle'), 'wb') as f:
+        pickle.dump(split_dict, f)
